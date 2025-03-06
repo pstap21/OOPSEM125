aa415a8668cab148c9be3c37b1ec4e2d45c9f103
pstap21 <a1629853@adelaide.edu.au>
2025-03-06 17:04:08 +1030

    update

---
+++ prac1/function-1-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int array_sum(int array [], int n){
+    int array_sum = 0;
+
+    if (n<1) {
+        return 0;
+    }
+
+    int sum =0;
+    for (int i =0; i < n; i++) {
+        sum += array [i]; //add each element of the array to sum
+    }
+}
+
---
+++ prac1/main-1-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//declar function
+int array_sum(int array [], int n);
+
+int main() {
+
+    int array[5] = {1, 2, 3, 4, 5};
+    std::cout << "The sum of the elements in the array is: " << array_sum(array, 5) <<std::endl;
+    
+}
