f72199e7dc51634819b499ec60dd0ab03420a1b4
pstap21 <a1629853@adelaide.edu.au>
2025-03-09 11:23:18 +1030

    Add files via upload

---
+++ main-2-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int min_element( int array[], int n);
+
+int main() {
+
+    int array1[5] = {2, 4, 6, 8, 10};
+    std::cout << " The minimum element in array1 is: " << min_element( array1, 5) << std::endl;
+
+    return 0;
+    
+}
